2afe245085a5f632d04163a6975d2828fcb39813
Will Lynas <43895423+will-lynas@users.noreply.github.com>
2024-10-27 23:25:15 +0000

    add square 2

--- week_1/question_1.py
+++ week_1/question_1.py
@@ -37,5 +37,21 @@ lt(90)
 fd(50)
 lt(90)
 
+# Square 2
+penup()
+setpos(-220, 0)
+pendown()
+
+pencolor("blue")
+
+forward(80)
+left(90)
+forward(80)
+lt(90)
+fd(80)
+lt(90)
+fd(80)
+lt(90)
+
 # leave the turtle on the screen until the user clicks in the screen
 exitonclick()
