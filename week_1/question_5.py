5785dbfbaec92f21e3bfe5b413c876acf2be4c48
Will Lynas <43895423+will-lynas@users.noreply.github.com>
2024-10-27 23:49:11 +0000

    solve q5

--- week_1/question_5.py
+++ week_1/question_5.py
@@ -20,14 +20,16 @@ speed(0)
 # Function to draw a polygon
 
 def draw_n_gon(sides, side_length):
-    pass
+    for _ in range(sides):
+        fd(side_length)
+        lt(360/sides)
     
 # Add your code here
 
 
 # place the turtle on the left hand side of your canvas
 penup()
-setpos(-300,0)
+setpos(-355,0)
 pendown()
 
 # set the pen width
@@ -37,7 +39,31 @@ pensize(2)
 pencolor("DarkOliveGreen4")
 
 # Draw triangle
-draw_n_gon(3, 50)
+draw_n_gon(3, 30)
+
+penup()
+setpos(-300, 0)
+pendown()
+pencolor("blue")
+draw_n_gon(4, 50)
+
+penup()
+setpos(-205, 0)
+pendown()
+pencolor("green")
+draw_n_gon(5, 70)
+
+penup()
+setpos(-45, 0)
+pendown()
+pencolor("red")
+draw_n_gon(6, 90)
+
+penup()
+setpos(170, 0)
+pendown()
+pencolor("pink")
+draw_n_gon(8, 105)
 
 # leave the turtle on the screen until the user clicks in the screen
 exitonclick()
